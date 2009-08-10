75e11cc60dee73bb7c520c4568048ac1fb3de0a5
Prashanth Mundkur <prashanth.mundkur@gmail.com>
2009-08-10 08:09:07 -0700

    Fix a typo in the Citrix license, and add the referenced LICENSE file.

--- codegen/json_convert/tests/test_json_conv.ml
+++ codegen/json_convert/tests/test_json_conv.ml
@@ -3,9 +3,9 @@
  * Author Prashanth Mundkur <firstname.lastname@citrix.com>
  *
  * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published
- * by the Free Software Foundation; version 2.1 only. with the special
- * exception on linking described in file LICENSE.
+ * it under the terms of the GNU Lesser General Public License (version
+ * 2.1 only) as published by the Free Software Foundation, with the
+ * special exception on linking described in file LICENSE.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
--- protocols/jsonrpc/json.ml
+++ protocols/jsonrpc/json.ml
@@ -1,12 +1,12 @@
 (*
- * Copyright (C) 2008-2009 Citrix Ltd.
+ * Copyright (C) 2009      Citrix Ltd.
  * Author Prashanth Mundkur <firstname.lastname@citrix.com>
  * Author Vincent Hanquez <vincent.hanquez@eu.citrix.com>
  *
  * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published
- * by the Free Software Foundation; version 2.1 only. with the special
- * exception on linking described in file LICENSE.
+ * it under the terms of the GNU Lesser General Public License (version
+ * 2.1 only) as published by the Free Software Foundation, with the
+ * special exception on linking described in file LICENSE.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
