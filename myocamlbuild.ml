73945eb9038c5c7e2d9ec7557b0fc5625509bc24
Prashanth Mundkur <prashanth.mundkur@gmail.com>
2011-01-15 16:22:08 -0800

    ocamlbuild: add remaining protocols (dbus, json/jsonrpc)

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -19,27 +19,41 @@ open Ocamlbuild_plugin
 
 let headers = ["eventloop/eventloop.h"];;
 let libeventloop = "eventloop/libeventloop." ^ !Options.ext_lib;;
+let libodbus = "protocols/dbus/libodbus." ^ !Options.ext_lib;;
 
 dispatch begin function
   | After_rules ->
       (* Handle the C stubs in eventloop *)
       flag ["link"; "library"; "ocaml"; "byte"; "use_libeventloop"]
-        (S[A"-dllib"; A"-leventloop"; A"-cclib"; A "-leventloop"]);
+        (S[A"-dllib"; A"-leventloop"; A"-cclib"; A"-leventloop"]);
       flag ["link"; "library"; "ocaml"; "native"; "use_libeventloop"]
         (S[A"-cclib"; A"-leventloop"]);
-      dep  ["link"; "ocaml"; "use_libeventloop"] [libeventloop];
+      dep ["link"; "ocaml"; "use_libeventloop"] [libeventloop];
       (* As an approximation, all C files use the headers.
          Note: This will import headers in the build directory. *)
-      dep  ["compile"; "c"] headers;
+      dep ["compile"; "c"] headers;
 
       (* Define the eventloop library and its link behaviour *)
       ocaml_lib ~dir:"eventloop" "eventloop/eventloop";
-      flag ["link"; "ocaml"; "native"; "use_eventloop"] (S[A"-I"; P"eventloop"]);
-      flag ["link"; "ocaml"; "byte"; "use_eventloop"] (S[A"-custom"; A"-I"; P"eventloop"]);
+      flag ["link"; "ocaml"; "byte"; "use_eventloop"]
+        (S[A"-custom"; A"-I"; P"eventloop"]);
+      flag ["link"; "ocaml"; "native"; "use_eventloop"]
+        (S[A"-I"; P"eventloop"]);
+
+      (* Handle the C stubs in dbus *)
+      ocaml_lib ~dir:"protocols/dbus" "protocols/dbus/dbuslib";
+      flag ["link"; "ocaml"; "byte"; "use_dbuslib"]
+        (S[A"-custom"; A"-I"; P"protocols/dbus";
+           A"-dllib"; A"-lodbus"; A"-cclib"; A"-lodbus"]);
+      flag ["link"; "ocaml"; "native"; "use_dbuslib"]
+        (S[A"-I"; P"protocols/dbus"; A"-cclib"; A"-lodbus"]);
+      dep ["link"; "ocaml"; "use_dbuslib"] [libodbus];
 
       (* Set up include paths *)
       Pathname.define_context "eventloop" ["eventloop"];
       Pathname.define_context "protocols/http" ["eventloop"; "protocols/http"];
       Pathname.define_context "protocols/http/tests" ["eventloop"; "protocols/http"];
+      Pathname.define_context "protocols/dbus" ["eventloop"];
+      Pathname.define_context "protocols/dbus/test" ["eventloop"; "protocols/dbus"];
   | _ -> ()
 end
