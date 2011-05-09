655cf4aee4083d8f82cda9cf7cbf3fefe3c69366
pmundkur <prashanth.mundkur@gmail.com>
2011-05-09 15:32:14 -0700

    Update json test script to skip inapplicable tests.

--- protocols/jsonrpc/tests/run_tests.sh
+++ protocols/jsonrpc/tests/run_tests.sh
@@ -2,21 +2,31 @@
 
 PROG=./test_parser
 
+function errmsg {
+    case $1 in
+	fail1.json|fail10.json|fail18.json)
+	    ;;
+	*)
+            echo $2
+	    ;;
+    esac
+}
+
 for f in `ls *pass*.json`; do
-	$PROG $f -no-print-value
-	ec=$?
-	if [ $ec -eq 1 ] ; then
-		echo "Test case $f should pass, but failed."
-	elif [ $ec -eq 255 ] ; then
-		echo "Test case $f triggered an internal error!"
-	fi
+    $PROG $f -no-print-value
+    ec=$?
+    if [ $ec -eq 1 ] ; then
+	errmsg $f "Test case $f should pass, but failed."
+    elif [ $ec -eq 255 ] ; then
+	errmsg $f "Test case $f triggered an internal error!"
+    fi
 done
 for f in `ls *fail*.json`; do
-	$PROG $f -no-print-value
-	ec=$?
-	if [ $ec -eq 0 ] ; then
-		echo "Test case $f should fail, but passed."
-	elif [ $ec -eq 255 ] ; then
-		echo "Test case $f triggered an internal error!"
-	fi
+    $PROG $f -no-print-value
+    ec=$?
+    if [ $ec -eq 0 ] ; then
+	errmsg $f "Test case $f should fail, but passed."
+    elif [ $ec -eq 255 ] ; then
+	errmsg $f "Test case $f triggered an internal error!"
+    fi
 done
