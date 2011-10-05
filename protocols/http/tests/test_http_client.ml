e2d15118239d9a893b61f5edb74cde4991b7e4c6
pmundkur <prashanth.mundkur@gmail.com>
2011-10-05 15:58:02 -0700

    Add a test for http head requests (and re-indent).

--- protocols/http/tests/test_http_client.ml
+++ protocols/http/tests/test_http_client.ml
@@ -21,8 +21,8 @@ module U = Unix
 
 let string_of_resp resp =
   let buf = Buffer.create 1024 in
-    H.Response.serialize buf resp;
-    Buffer.contents buf
+  H.Response.serialize buf resp;
+  Buffer.contents buf
 
 let string_of_req = function
   | Client.Payload (_, _) -> "payload"
@@ -43,22 +43,23 @@ let funopt f = function
 let show_result verbose r =
   Printf.printf "\n%s %s: " (H.string_of_meth r.Client.meth) r.Client.url;
   (match r.Client.response with
-     | None ->
-         Printf.printf "No response\n"
-     | Some resp ->
-         Printf.printf "Response received\n";
-         if verbose then Printf.printf "%s\n" (string_of_resp resp);
+    | None ->
+      Printf.printf "No response\n"
+    | Some resp ->
+      Printf.printf "Response received\n";
+      if verbose || r.Client.meth = H.Head then Printf.printf "%s\n" (string_of_resp resp);
   );
   funopt (fun el -> List.iter errmsg el) r.Client.error
 
 let rec show_results verbose = function
   | [] -> ()
   | r :: rest ->
-      show_result verbose r;
-      show_results verbose rest
+    show_result verbose r;
+    show_results verbose rest
 
 let print_usage () =
   Printf.printf "%s: [options]\n" Sys.argv.(0);
+  Printf.printf "    -head url+ : print the headers of the url\n";
   Printf.printf "    -get url+ : print the contents of the url\n";
   Printf.printf "    -save file url+ : save the contents of the url to a file\n";
   Printf.printf "    (-put|-post) file url+ : put the contents of the file at the url\n";
@@ -75,6 +76,9 @@ let run () =
   let get_url urls =
     List.iter check_supported_url urls;
     reqs := (H.Get, Client.Payload (urls, None), 0) :: !reqs in
+  let head_url urls =
+    List.iter check_supported_url urls;
+    reqs := (H.Head, Client.Payload (urls, None), 0) :: !reqs in
   let send_url meth urls filename =
     List.iter check_supported_url urls;
     let fd = U.openfile filename [U.O_RDONLY] 0 in
@@ -101,33 +105,37 @@ let run () =
     if indx >= num_args then ()
     else (let opt = Sys.argv.(indx) in
             match opt with
+              | "-head" ->
+                let urls, next = get_url_args (indx + 1) in
+                head_url urls;
+                process_args next
               | "-get" ->
-                  let urls, next = get_url_args (indx + 1) in
-                    get_url urls;
-                    process_args next
+                let urls, next = get_url_args (indx + 1) in
+                get_url urls;
+                process_args next
               | "-put" | "-post" ->
-                  let meth = meth_of_opt opt in
-                  let file = get_arg opt (indx + 1) in
-                  let urls, next = get_url_args (indx + 2) in
-                    send_url meth urls file;
-                    process_args next
+                let meth = meth_of_opt opt in
+                let file = get_arg opt (indx + 1) in
+                let urls, next = get_url_args (indx + 2) in
+                send_url meth urls file;
+                process_args next
               | "-save" ->
-                  let file = get_arg opt (indx + 1) in
-                  let urls, next = get_url_args (indx + 2) in
-                    save_url urls file;
-                    process_args next
+                let file = get_arg opt (indx + 1) in
+                let urls, next = get_url_args (indx + 2) in
+                save_url urls file;
+                process_args next
               | "-h" | "--help" ->
-                  print_usage ()
+                print_usage ()
               | "-p" | "--payload" ->
-                  verbose := true
+                verbose := true
               | _ ->
-                  Printf.printf "Unrecognized option: %s\n" opt;
-                  print_usage ()
+                Printf.printf "Unrecognized option: %s\n" opt;
+                print_usage ()
          )
   in
-    process_args 1;
-    let results = Client.request !reqs
-    in show_results !verbose results
+  process_args 1;
+  let results = Client.request !reqs
+  in show_results !verbose results
 
 let _ =
   if Array.length Sys.argv == 1 then
@@ -136,10 +144,10 @@ let _ =
   try run ()
   with
     | Client.Invalid_url (url, e) ->
-        Printf.eprintf "Invalid url %s: %s\n%!" url e
+      Printf.eprintf "Invalid url %s: %s\n%!" url e
     | Client.Invalid_request r ->
-        Printf.eprintf "Invalid %s request: no urls!\n%!" (string_of_req r)
+      Printf.eprintf "Invalid %s request: no urls!\n%!" (string_of_req r)
     | e ->
-        Printf.eprintf "%s\n%s\n%!"
-          (Printexc.to_string e)
-          (Printexc.get_backtrace ())
+      Printf.eprintf "%s\n%s\n%!"
+        (Printexc.to_string e)
+        (Printexc.get_backtrace ())
